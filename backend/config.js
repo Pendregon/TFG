0e352eedac5793a1b4d10abbb65246a8d1cf8da5
pendregon <Lander_isleta@hotmail.com>
2021-07-14 11:12:41 +0100

    fix connection to bbdd

--- backend/config.js
+++ backend/config.js
@@ -2,10 +2,9 @@ const env = process.env;
 
 const config = {
     db: { /* don't expose password or any sensitive info, done only for demo */
-        connectionLimit : 10000,
-        host: env.DB_HOST || 'localhost',
-        user: env.DB_USER || 'root',
-        password: env.DB_PASSWORD || '',
+        host: env.DB_HOST || 'tfg.cj3lzrzgxvol.eu-west-3.rds.amazonaws.com',
+        user: env.DB_USER || 'admin',
+        password: env.DB_PASSWORD || '4AE?j=D<yk&zbU7Q&?&UF}=A:4_r4]F;',
         database: env.DB_NAME || 'tfg',
     },
     listPerPage: env.LIST_PER_PAGE || 20,
