d91fe63a4968e1de85dc3632db58553f0d668247
pendregon <Lander_isleta@hotmail.com>
2021-10-11 11:06:47 +0100

    updated db connection

--- backend/config.js
+++ backend/config.js
@@ -2,9 +2,9 @@ const env = process.env;
 
 const config = {
     db: { /* don't expose password or any sensitive info, done only for demo */
-        host: env.DB_HOST || 'tfg.cj3lzrzgxvol.eu-west-3.rds.amazonaws.com',
-        user: env.DB_USER || 'admin',
-        password: env.DB_PASSWORD || '4AE?j=D<yk&zbU7Q&?&UF}=A:4_r4]F;',
+        host: env.DB_HOST || 'localhost',
+        user: env.DB_USER || 'root',
+        password: env.DB_PASSWORD || '',
         database: env.DB_NAME || 'tfg',
     },
     listPerPage: env.LIST_PER_PAGE || 20,
