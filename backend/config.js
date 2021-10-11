d9871b123f8e237cc577ae4b90d286e551bfa113
pendregon <Lander_isleta@hotmail.com>
2021-10-11 18:45:08 +0100

    changing db port

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@ const env = process.env;
 
 const config = {
     db: { /* don't expose password or any sensitive info, done only for demo */
-        host: env.DB_HOST || 'localhost',
+        host: env.DB_HOST || 'localhost:3306',
         user: env.DB_USER || 'root',
         password: env.DB_PASSWORD || '',
         database: env.DB_NAME || 'tfg',
