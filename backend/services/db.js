2f3a887ae700f30be9fa1350802dda9855fac8de
pendregon <Lander_isleta@hotmail.com>
2021-10-14 09:51:31 +0100

    solving db problem

--- backend/services/db.js
+++ backend/services/db.js
@@ -2,10 +2,20 @@ const mysql = require('mysql2/promise');
 const config = require('../config');
 let connection = null
 async function query(sql, params) {
-    if(!connection || connection.state === 'closed' || connection.state === 'disconnected')
+    if(!connection){
         connection = await mysql.createConnection(config.db);
+    }else{
+        let disconnected = false
+        connection.ping(err => {
+            if(err)
+                disconnected = true
+        });
+        if(disconnected) {
+            console.log("VOLVEMOS A CONECTAR")
+            connection = await mysql.createConnection(config.db);
+        }
+    }
     const [results, ] = await connection.execute(sql, params);
-
     return results;
 }
 
