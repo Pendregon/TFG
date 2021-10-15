2cb9641d9361e5f08d8b8469da1df16073e59574
pendregon <Lander_isleta@hotmail.com>
2021-10-15 10:26:57 +0100

    solved bbdd connection

--- backend/services/db.js
+++ backend/services/db.js
@@ -4,16 +4,10 @@ let connection = null
 async function query(sql, params) {
     if(!connection){
         connection = await mysql.createConnection(config.db);
-    }else{
-        let disconnected = false
-        connection.ping(err => {
-            if(err)
-                disconnected = true
-        });
-        if(disconnected) {
-            console.log("VOLVEMOS A CONECTAR")
-            connection = await mysql.createConnection(config.db);
-        }
+        setTimeout(function () {
+            connection.end()
+            connection = null
+        }, 1800000)
     }
     const [results, ] = await connection.execute(sql, params);
     return results;
