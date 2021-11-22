86830d1700c4ba6f4fd699117c6e856128c6f38d
Pendregon <jon.lander.torres@gmail.com>
2021-11-22 13:55:34 +0000

    bbdd data

--- backend/config.js
+++ backend/config.js
@@ -3,8 +3,8 @@ const env = process.env;
 const config = {
     db: { /* don't expose password or any sensitive info, done only for demo */
         host: env.DB_HOST || '127.0.0.1',
-        user: env.DB_USER || 'root',
-        password: env.DB_PASSWORD || '',
+        user: env.DB_USER || 'administrador',
+        password: env.DB_PASSWORD || 'JamonSerrano1*',
         database: env.DB_NAME || 'tfg',
     },
     listPerPage: env.LIST_PER_PAGE || 20,
