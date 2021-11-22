c9228cceab901c1389ca5526d6f99083f49a2cab
Pendregon <jon.lander.torres@gmail.com>
2021-11-22 14:20:32 +0000

    fixed search add button

--- frontend/src/components/searchMission/SearchMissionElement.vue
+++ frontend/src/components/searchMission/SearchMissionElement.vue
@@ -19,7 +19,7 @@
 						<path d="M13,9H11V7H13M13,17H11V11H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
 					</svg>
 				</button>
-				<button @click="$emit('add', mission)" :disabled="!can_add || checkIfMissionButtonIsDisabled(mission)">
+				<button @click="$emit('add', mission)" :disabled="!can_add">
 					<svg viewBox="0 0 24 24">
 						<path d="M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
 					</svg>
